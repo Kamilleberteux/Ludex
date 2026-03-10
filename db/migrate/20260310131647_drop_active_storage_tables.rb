5a2c1cc6ba2354385eb84c2a2d95ef4594d258c9
Olivier Giorno <olivier.giorno@gmail.com>
2026-03-10 15:39:33 +0100

    reversing migrations, WIP

--- db/migrate/20260310131647_drop_active_storage_tables.rb
+++
@@ -1,7 +0,0 @@
-class DropActiveStorageTables < ActiveRecord::Migration[8.1]
-  def change
-    drop_table :active_storage_variant_records
-    drop_table :active_storage_attachments
-    drop_table :active_storage_blobs
-  end
-end
