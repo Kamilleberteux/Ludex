431bd3e1c200e9ec2024974c180ce43b7917bad0
Romain Gateaud <romaingateaud@hotmail.com>
2026-03-16 12:58:27 +0100

    changement et rectification

--- app/models/collection_game.rb
+++ app/models/collection_game.rb
@@ -1,4 +1,6 @@
 class CollectionGame < ApplicationRecord
   belongs_to :collection
   belongs_to :game
+
+  validates :game_id, uniqueness: { scope: :collection_id, message: "Ce jeu est déjà dans cette collection" }
 end
