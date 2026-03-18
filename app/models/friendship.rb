15ffa31d6939eb8e9c27c052686dbc84c155bff6
camille benteux <benteuxc@gmail.com>
2026-03-18 16:01:03 +0100

    Few corrections

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,6 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: "User"
-  validates :friend, uniqueness: true
+  validates :friend_id, uniqueness: { scope: :user_id }
 end
 
